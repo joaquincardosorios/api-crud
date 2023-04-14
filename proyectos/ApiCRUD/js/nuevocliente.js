93696342abd5fbdc890e81d0f62e08d4b6a00f74
Joaquin Cardoso Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-14 12:14:24 -0400

    front-validation

---
+++ proyectos/ApiCRUD/js/nuevocliente.js
@@ -0,0 +1,36 @@
+import { mostrarAlerta} from './funciones.js'
+
+( function(){
+    const formulario = document.querySelector('#formulario');
+    formulario.addEventListener('submit', validarCliente);
+
+
+
+    function validarCliente(e){
+        e.preventDefault();
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
+        
+        if(validar(cliente)){
+            // Mensaje
+            mostrarAlerta('Todos los campos son obligatorios');
+            return;
+        }
+        console.log('paso la validacion')
+
+        
+    }
+
+    function validar(obj){
+        return !Object.values(obj).every( input => input !== '')
+    }
+})()
